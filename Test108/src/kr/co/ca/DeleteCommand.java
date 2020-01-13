9554d212bcdd04445001df2aa8c50ae5e5953f14
hjy9427 <59682177+hjy9427@users.noreply.github.com>
2020-01-13 17:41:53 +0900

    Update DeleteCommand.java

--- Test108/src/kr/co/ca/DeleteCommand.java
+++ Test108/src/kr/co/ca/DeleteCommand.java
@@ -6,17 +6,24 @@ public class DeleteCommand implements Command {
 
 	@Override
 	public void execute(Scanner sc) {
-		
-		System.out.println("ȸ�� ���� ����");
-		
-		System.out.println("ID�� �Է��ϼ���");
-		String id = sc.nextLine();
-		
-		MemberDAO dao = new MemberDAO();
-		
-	
-		dao.delete(id);
+		System.out.println("회원 정보 삭제");
+	    System.out.println("ID를 입력하세요");
+	    
+	    while(true) {
+	    String id = sc.nextLine();
+	    MemberDAO dao = new MemberDAO();
+	    MemberDTO member = dao.selectById(id);
+	        if(member == null) {
+	        	System.out.println("없는 아이디입니다 다른 아이디를 입력하세요");
+	        } else {
+	        	dao.delete(id);
+	        	System.out.println("삭제되었습니다");
+	        	break;
+	        }
+
+	    }
 
 	}
 
 }
+
